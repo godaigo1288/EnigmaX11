2eb9e1b572b3bd488a4596941675d73e5d41327e
godaigo1288 <godaigo1288@gmail.com>
2015-04-02 18:57:36 -0500

    $XEN_X11
    Launch!!!!

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define EnigmaX11-qt message queue name
+#define BITCOINURI_QUEUE_NAME "EnigmaX11URI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
